fd6091eb672a88c1d83c0e7031b6ccf1ece25faa
Paul <theapplegates@users.noreply.github.com>
2020-06-28 15:18:32 -0400

    chnages

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,11 +24,11 @@ module.exports = {
         externalLinks: [
           {
             name: `Twitter`,
-            url: `https://twitter.com/lekoarts_de`,
+            url: `https://twitter.com/papplegate`,
           },
           {
             name: `Instagram`,
-            url: `https://www.instagram.com/lekoarts.de/`,
+            url: `https://www.instagram.com/mrapplegate/`,
           },
         ],
       },
@@ -43,9 +43,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-        short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        name: `Living Life`,
+        short_name: `Living Life`,
+        description: `Living life and writing some things down along the way.`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#6B46C1`,
